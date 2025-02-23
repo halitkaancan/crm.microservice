957439b0f5f45066dcab58327951d98452141b62
Kaan <halitkaancan@gmail.com>
2025-02-23 23:45:45 +0300

    latest pom.xml

---
+++ common-v1/target/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"io.github.halitkaancan","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"io.github.halitkaancan","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
