36dd6a29ac65c862a7b70b4f688a568ae27528c9
Kaan <halitkaancan@gmail.com>
2025-03-15 17:05:57 +0300

    dockerization

--- order-service/src/main/java/com/hkc/orderservice/controller/OrdersController.java
+++ order-service/src/main/java/com/hkc/orderservice/controller/OrdersController.java
@@ -2,30 +2,38 @@ package com.hkc.orderservice.controller;
 
 import com.hkc.orderservice.client.ProductClient;
 import com.hkc.orderservice.entitiy.Order;
+import io.github.halitkaancan.order.OrderCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 
 @RestController
 @RequestMapping("/api/v1/orders")
-@RequiredArgsConstructor
 public class OrdersController {
 
     private final ProductClient productClient;
     private final StreamBridge streamBridge;
+    public OrdersController(ProductClient productClient, StreamBridge streamBridge) {
+        this.productClient = productClient;
+        this.streamBridge = streamBridge;
+    }
 
     @GetMapping
-    public String order() {
-//        String response = productClient.get();
-//        System.out.println(response);
+    public String get() {
+        // Syncİletişim
+        //String response = productClient.get();
+        //System.out.println(response);
 
-        Order order = new Order();
-        order.setId("abc123");
-        streamBridge.send("orderCreatedFunction-out-0","Mesaj iletildi...");
-        return "Order service is running";
+        OrderCreatedEvent orderCreatedEvent = new
+                OrderCreatedEvent("abc123", LocalDate.now());
+
+        streamBridge.send("orderCreatedFunction-out-0", orderCreatedEvent);
+        return "Order Service";
     }
 }
 
--- order-service/src/main/java/com/hkc/orderservice/entitiy/Order.java
+++ order-service/src/main/java/com/hkc/orderservice/entitiy/Order.java
@@ -1,15 +1,29 @@
 package com.hkc.orderservice.entitiy;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
 
+import java.time.LocalDate;
+
+@Entity
+@Table(name ="orders")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
+    @Id
+    @UuidGenerator
     private String id;
+
+    @Column(name="order_date")
+    private LocalDate orderDate;
 
 }
